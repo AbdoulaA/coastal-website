4bf560eeccf1f02d7677333cfadf98d5408a459e
Abdorruhman Abdelwahed <206abdoula@gmail.com>
2024-01-22 00:11:06 -0500

    switching activated

--- app.js
+++ app.js
@@ -1,11 +1,60 @@
-// function to detect which side of the screen the cursor is on
 
-function detectCursorSide() {
-    window.addEventListener('mousemove', function(event) {
-        if (event.clientX < window.innerWidth / 2) {
-            document.querySelector('header').style.backgroundImage = 'url("./assets/light tower coastal right.png")';
-        } else {
-            document.querySelector('header').style.backgroundImage = 'url("./assets/light tower coastal left.png")';
-        }
-    });
+let isRightImage = true;
+
+function detectSide() {
+
+    if (isRightImage) {
+        document.querySelector('header').style.backgroundImage = 'url("./assets/light tower coastal left.png")';
+        document.querySelector('.header__btns').style = 
+        `position: absolute;
+        top: 40%;
+        left: 20%;
+        height: 75px;
+        background-color:#0f3252;
+        border-radius: 15px;
+        display: flex;
+        justify-content: center;
+        align-items: center;`;
+        
+        document.querySelector('.header__desc').style = `
+        position: absolute;
+        top: 20%;
+        right: 5%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        `;
+
+        document.querySelector('.header__desc--title').innerHTML = `<h1 class="header__desc--title">Clients</h1>`
+        document.querySelector('.btn').innerHTML = `<a href="#" class="btn">Companies</a>`
+
+    } else {
+        document.querySelector('header').style.backgroundImage = 'url("./assets/light tower coastal right.png")';
+        document.querySelector('.header__btns').style = 
+        `position: absolute;
+        top: 40%;
+        right: 20%;
+        height: 75px;
+        background-color:#0f3252;
+        border-radius: 15px;
+        display: flex;
+        justify-content: center;
+        align-items: center;`;
+
+        document.querySelector('.header__desc').style = `
+        position: absolute;
+        top: 20%;
+        left: 5%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        `;
+
+        document.querySelector('.header__desc--title').innerHTML = `<h1 class="header__desc--title">Companies</h1>`
+        document.querySelector('.btn').innerHTML = `<a href="#" class="btn">Clients</a>`
+
+
+    }
+    isRightImage = !isRightImage;
 }
+
